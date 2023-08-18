8cb92d59e05cc8b7c0f4bdcdb087845f97be6627
Artem Shishkin <a.shishkin02@gmail.com>
2023-08-19 02:11:24 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './App.module.css';
 import TodoForm from './components/TodoForm/TodoForm';
